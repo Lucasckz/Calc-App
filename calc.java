202a1295ab7ac9549f28ed22ace991b4bbf622bb
Lucas Keizur <Lucasckz@yahoo.com>
2021-10-19 19:26:26 -0700

    10/19/2021 - Second official Commit adds some structure to code.

--- calc.java
+++ calc.java
@@ -1,8 +1,24 @@
 
+import java.util.*;
+
 public class calc{
     public static void main(String[] args){
+        
+        System.out.println("Please type out your equation: ");
 
-        System.out.println("Hello World!");
+        Scanner keyboard = new Scanner(System.in);
+        String input = keyboard.nextLine();
+        keyboard.close();
+
+
+        System.out.println(input);
 
     }
+
+    public static void solve(String in){
+        
+    }
+
+
+
 }
