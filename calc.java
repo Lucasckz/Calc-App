37c1d90553168cd886dba299b4339783a7a2442b
Lucas Keizur <Lucasckz@yahoo.com>
2021-10-25 11:16:02 -0700

    10/25/2021 Finalized many functions; structure is complete

--- calc.java
+++ calc.java
@@ -28,12 +28,9 @@ public class calc{
         List<String> out = new ArrayList<String>();
         int i = 0;
         int p;
+        List<String> wordBank = List.of("SIN", "COS", "TAN");
         while (i < in.length()) {
             if (in.charAt(i)==' '){
-                i++;
-            }
-            if ( in.charAt(i)=='(' || in.charAt(i)==')' ){
-                out.add(in.substring(i, i+1));
                 i++;
             }
             else if ( Character.isDigit(in.charAt(i)) ){
@@ -45,12 +42,20 @@ public class calc{
             }
             else if ( Character.isAlphabetic(in.charAt(i)) ) {
                 p = i;
-                while (i < in.length() && Character.isAlphabetic(in.charAt(i))){
+                boolean head = true;
+                while (i < in.length() && Character.isAlphabetic(in.charAt(i)) && head){
+                    if ( wordBank.contains(in.substring(p, i+1).toUpperCase()) ){
+                        out.add(in.substring(p, i+1).toUpperCase());
+                        head = false;
+                    }
                     i++;
+                    if ((i == in.length() || !Character.isAlphabetic(in.charAt(i)))  && head ) {throw new NullPointerException();}
                 }
-                out.add(in.substring(p, i));
             }
-            else if (  (in.charAt(i)=='-' && i == 0) || (in.charAt(i)=='-' && !Character.isDigit(in.charAt(i-1)))  ){
+            else if (  (in.charAt(i)=='-' && i == 0) || (in.charAt(i)=='-' && (!Character.isDigit(in.charAt(i-1)) 
+                        && !(in.charAt(i-1)== ')' || in.charAt(i-1)== ']' || in.charAt(i-1)== '}' )
+                        && !(in.charAt(i+1)== ')' || in.charAt(i+1)== ']' || in.charAt(i+1)== '}' ))) 
+                        && i != in.length()-1 ){
                 out.add("-1");
                 i++;
             }
@@ -66,30 +71,49 @@ public class calc{
 
         //Mask
         int k;
+        String open;
+        String close;
         for(int i = 0; i < in.size(); i++){
             //Deals with Paranthe
             k = i;
-            if ( in.get(i).equals("(") ){
+            if ( in.get(i).equals("(") || in.get(i).equals("{") || in.get(i).equals("[") ){
+                open = in.get(i);
+                if (open.equals("(")) {close = ")";}
+                else if (open.equals("{")) {close = "}";}
+                else {close = "]";}
+                List<String> newList = new ArrayList<String>();
                 int p = 1;
-                while(p != 0){
+                while(p > 0){
                     k++;
-                    if ( in.get(k).equals("(") ){
+                    if ( in.get(k).equals(open) ){
                         p++;
                     }
-                    else if ( in.get(k).equals(")") ) {
+                    else if ( in.get(k).equals(close) ) {
                         p--;
                     }
+                    newList.add(in.get(k));
                 }
-                in.add( i,  String.valueOf(solve(in.subList(i+1, k))));
-                for (int j = 0; j <= (k-i); j++) {
+                newList.remove(newList.size()-1);
+                int z = newList.size()+2;
+                in.add( i,  String.valueOf(solve(newList)));
+                for (int j = 0; j < z; j++) {
+                    if (in.size()==1) {break;}
                     in.remove(i+1);
                 }
             }
-            else if(in.get(i).equals("+") || in.get(i).equals("-")){
+            else if(in.get(i).equals("+")){
                 return solve(in.subList(0, i)) + solve(in.subList(i+1, in.size()));
             }
             else if (in.get(i).equals("-")) {
                 return solve(in.subList(0, i)) - solve(in.subList(i+1, in.size()));
+            }
+        }
+        
+        for (int i = 0; i < in.size(); i++) {
+            if (in.get(i).equals("^")){
+                in.add(i-1, String.valueOf(Math.pow( Double.parseDouble(in.get(i-1)), Double.parseDouble(in.get(i+1))) ) );
+                in.remove(i);
+                in.remove(i);
             }
         }
         return mult(in);
@@ -98,7 +122,6 @@ public class calc{
     
 
     public static Double mult(List<String> in){
-        //System.out.println(3);
         if (in.size()==1) { 
             return Double.parseDouble(in.get(0));
         }
@@ -108,9 +131,31 @@ public class calc{
         else if(in.get(in.size()-2).equals("*"))    { 
             return mult(in.subList(0, in.size()-2)) * Double.parseDouble(in.get(in.size()-1)); 
         }
+        else if(in.get(in.size()-2).equals("SIN"))  {
+            if (in.size()==2) {
+               return Math.sin(Double.parseDouble(in.get(in.size()-1)));
+            }
+            else{
+                in.add( in.size()-2, String.valueOf(mult(in.subList(in.size()-2, in.size()))) );
+                in.remove(in.size()-1);
+                in.remove(in.size()-1);
+                return mult(in);
+            }
+        }
         else if(in.get(in.size()-2).equals("COS"))  {
             if (in.size()==2) {
                 return Math.cos(Double.parseDouble(in.get(in.size()-1)));
+            }
+            else{
+                in.add( in.size()-2, String.valueOf(mult(in.subList(in.size()-2, in.size()))) );
+                in.remove(in.size()-1);
+                in.remove(in.size()-1);
+                return mult(in);
+            }
+        }
+        else if(in.get(in.size()-2).equals("TAN"))  {
+            if (in.size()==2) {
+                return Math.tan(Double.parseDouble(in.get(in.size()-1)));
             }
             else{
                 in.add( in.size()-2, String.valueOf(mult(in.subList(in.size()-2, in.size()))) );
@@ -130,7 +175,7 @@ public class calc{
             }
         }
         else{
-            return Double.parseDouble(in.get(0));
+            throw new NullPointerException();
         }
     }
 }
