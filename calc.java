852c0ab7e0df873f69b4748d69ffafee5510601a
Lucas Keizur <Lucasckz@yahoo.com>
2021-10-25 07:22:13 -0700

    10/24/2021 forgot to commit last night - implemented first functions.

--- calc.java
+++ calc.java
@@ -12,12 +12,15 @@ public class calc{
         String input = keyboard.nextLine();
         keyboard.close();
 
-        //List<String> a = toList(input);
-        //for (int i = 0; i < a.size(); i++){
-        //    System.out.print( a.get(i) + " ! " );
-        //}
-
-        System.out.println("= " + solve(toList(input)));
+        try{
+            Double solution;
+            List<String> toke = toList(input);
+            solution = solve(toke);
+            System.out.println("Solution:  " + solution);
+        }
+        catch(Exception e){
+            System.out.println("Improper Format...  Please Try Again");
+        }
     }
 
 //  Converts the string to a List pf type String
@@ -35,7 +38,7 @@ public class calc{
             }
             else if ( Character.isDigit(in.charAt(i)) ){
                 p = i;
-                while (i < in.length() && Character.isDigit(in.charAt(i))){
+                while (i < in.length() && ( Character.isDigit(in.charAt(i)) || in.charAt(i)=='.') ){
                     i++;
                 }
                 out.add(in.substring(p, i));
@@ -47,6 +50,10 @@ public class calc{
                 }
                 out.add(in.substring(p, i));
             }
+            else if (  (in.charAt(i)=='-' && i == 0) || (in.charAt(i)=='-' && !Character.isDigit(in.charAt(i-1)))  ){
+                out.add("-1");
+                i++;
+            }
             else {
                 out.add(in.substring(i, i+1));
                 i++;
@@ -54,27 +61,76 @@ public class calc{
         }
         return out;
     }
+    //Solves the given equation
+    public static Double solve(List<String> in){
 
-    public static double solve(List<String> in){
-        return 0;
-    }
-
-    public static void recurse(String in){
-    }
-
-    public static void breakUp(String in){
-    }
-
-
-    //method to take in two Strings and combine them based on the middle String
-    public static double combine(String a, String b, String c){
-        if (c.equals("")) {
-            return Double.parseDouble(a);
+        //Mask
+        int k;
+        for(int i = 0; i < in.size(); i++){
+            //Deals with Paranthe
+            k = i;
+            if ( in.get(i).equals("(") ){
+                int p = 1;
+                while(p != 0){
+                    k++;
+                    if ( in.get(k).equals("(") ){
+                        p++;
+                    }
+                    else if ( in.get(k).equals(")") ) {
+                        p--;
+                    }
+                }
+                in.add( i,  String.valueOf(solve(in.subList(i+1, k))));
+                for (int j = 0; j <= (k-i); j++) {
+                    in.remove(i+1);
+                }
+            }
+            else if(in.get(i).equals("+") || in.get(i).equals("-")){
+                return solve(in.subList(0, i)) + solve(in.subList(i+1, in.size()));
+            }
+            else if (in.get(i).equals("-")) {
+                return solve(in.subList(0, i)) - solve(in.subList(i+1, in.size()));
+            }
         }
-        else if (b.equals("")) {
-            return Double.parseDouble(a)*Double.parseDouble(b);
-        }
+        return mult(in);
+    }
 
-        return 0;
+    
+
+    public static Double mult(List<String> in){
+        //System.out.println(3);
+        if (in.size()==1) { 
+            return Double.parseDouble(in.get(0));
+        }
+        else if(in.get(in.size()-2).equals("/"))    {
+            return mult(in.subList(0, in.size()-2)) / Double.parseDouble(in.get(in.size()-1)); 
+        }
+        else if(in.get(in.size()-2).equals("*"))    { 
+            return mult(in.subList(0, in.size()-2)) * Double.parseDouble(in.get(in.size()-1)); 
+        }
+        else if(in.get(in.size()-2).equals("COS"))  {
+            if (in.size()==2) {
+                return Math.cos(Double.parseDouble(in.get(in.size()-1)));
+            }
+            else{
+                in.add( in.size()-2, String.valueOf(mult(in.subList(in.size()-2, in.size()))) );
+                in.remove(in.size()-1);
+                in.remove(in.size()-1);
+                return mult(in);
+            }
+        }
+        else if ( Character.isDigit( in.get(in.size()-2).charAt(0) )
+                                || ((in.get(in.size()-2).length() > 1) && Character.isDigit(in.get(in.size()-2).charAt(1)) )
+                                || ((in.get(in.size()-2).length() > 2) && Character.isDigit(in.get(in.size()-2).charAt(2)) )){
+            if (in.size()==2){
+                return Double.parseDouble(in.get(0)) * Double.parseDouble(in.get(1));
+            }
+            else{
+                return mult(in.subList(0, in.size()-1)) * Double.parseDouble(in.get(in.size()-1));
+            }
+        }
+        else{
+            return Double.parseDouble(in.get(0));
+        }
     }
 }
